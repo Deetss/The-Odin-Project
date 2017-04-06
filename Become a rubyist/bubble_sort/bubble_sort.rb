08ffaca9b044de0a89d0fdecf1cf1315a4117ede
Deetss <dylan817@live.com>
2017-04-06 01:42:49 -0500

    Finish bubble_sort & bubble_sort_by

--- Become a rubyist/bubble_sort/bubble_sort.rb
+++ Become a rubyist/bubble_sort/bubble_sort.rb
@@ -10,8 +10,9 @@ def bubble_sort (array)
       end
     end
   end
-  puts array
+  print array
 end
+
 def bubble_sort_by array, &block
   #work in progress
     max_loop = array.length - 1
@@ -19,13 +20,17 @@ def bubble_sort_by array, &block
     while swapped
       swapped = false
       for index in (1..max_loop)
-        if array[index - 1] > array[index]
+        if array[index - 1] < array[index]
+          yield array
           array[index - 1] , array[index] = array[index], array[index - 1]
           swapped = true
         end
       end
     end
-    puts array
-
+    print array
 end
 bubble_sort([4,3,78,2,0,2])
+puts " "
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+     left.length - right.length
+end

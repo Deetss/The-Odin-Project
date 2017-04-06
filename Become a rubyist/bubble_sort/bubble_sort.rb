d97ed6a3d2a46940b4612cba39c27fb94dd5f31a
Deetss <dylan817@live.com>
2017-04-06 01:28:29 -0500

    Create base for bubble_sort_by

--- Become a rubyist/bubble_sort/bubble_sort.rb
+++ Become a rubyist/bubble_sort/bubble_sort.rb
@@ -14,6 +14,18 @@ def bubble_sort (array)
 end
 def bubble_sort_by array, &block
   #work in progress
+    max_loop = array.length - 1
+    swapped = true
+    while swapped
+      swapped = false
+      for index in (1..max_loop)
+        if array[index - 1] > array[index]
+          array[index - 1] , array[index] = array[index], array[index - 1]
+          swapped = true
+        end
+      end
+    end
+    puts array
 
 end
 bubble_sort([4,3,78,2,0,2])

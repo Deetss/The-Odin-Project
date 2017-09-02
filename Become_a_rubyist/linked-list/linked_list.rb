505f3cfc8916ca4dfcb61cdb3ea9a82130f8a6ee
Deetss <dylan817@live.com>
2017-09-01 20:50:31 -0500

    Adds append method

--- Become_a_rubyist/linked-list/linked_list.rb
+++ Become_a_rubyist/linked-list/linked_list.rb
@@ -20,15 +20,17 @@ class LinkedList
     #Adds new node to the end of the list
     def append(value)
         if head == nil
-            self.prepend(value)
+            prepend(value)
         else
             tmp = head
-            while tmp != null
+            while tmp.next_node != nil
                 tmp = tmp.next_node
             end
-
+            tmp.next_node = Node.new(value)
+        end
     end
-    #Adds new Node to begining of listS
+
+    #Adds new Node to begining of list
     def prepend(value)
         @head = Node.new(value, head)
     end
@@ -36,3 +38,8 @@ class LinkedList
 end
 
 list = LinkedList.new
+list.append("a")
+list.append("b")
+list.prepend(1)
+list.append(100)
+p list

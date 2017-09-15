9a1456b534b5896075081a017a4456bf03dce70a
Deetss <dylan817@live.com>
2017-09-14 22:52:31 -0500

    Adds color changing to the background when you guess

--- Ruby_on_rails/sinatra/web_guesser/web_guesser.rb
+++ Ruby_on_rails/sinatra/web_guesser/web_guesser.rb
@@ -2,17 +2,23 @@ require "sinatra"
 require "sinatra/reloader"
 
 SECRET_NUM = rand(100) + 1
+$color = "white"
 
 def check_guess(guess)
     if guess - SECRET_NUM > 5
+        $color = "red"
         "Way too high!"
     elsif SECRET_NUM - guess > 5
+        $color = "red"
         "Way too low!"
     elsif guess > SECRET_NUM
+        $color = "#fcf235"
         "Too high!"
     elsif guess < SECRET_NUM
+        $color = "#fcf235"
         "Too low!"
     elsif guess == SECRET_NUM
+        $color = "green"
         "You got it right! <br> <br>The secret number is #{SECRET_NUM}"
     end
 end
@@ -20,6 +26,6 @@ end
 get "/" do
     guess = params['guess'].to_i
     message = check_guess(guess)
-    erb :index, :locals => {:message => message, :num => SECRET_NUM}
+    erb :index, :locals => {:message => message, :num => SECRET_NUM, :color => $color}
 
 end

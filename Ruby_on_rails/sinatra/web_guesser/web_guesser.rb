368d74b8e5258bf897e45fad2daa25f7fff85922
Deetss <dylan817@live.com>
2017-09-14 23:19:24 -0500

    Displays a message and restarts game if you run out of guesses or guess the correct number

--- Ruby_on_rails/sinatra/web_guesser/web_guesser.rb
+++ Ruby_on_rails/sinatra/web_guesser/web_guesser.rb
@@ -1,31 +1,65 @@
 require "sinatra"
 require "sinatra/reloader"
 
-SECRET_NUM = rand(100) + 1
-$color = "white"
+@@secret_num = rand(100) + 1
+@@correct = false
+@@guesses_left = 5
+@@color = "white"
 
+def pick_num
+    @@secret_num = rand(100) + 1
+end
 def check_guess(guess)
-    if guess - SECRET_NUM > 5
-        $color = "red"
+    if guess - @@secret_num > 5
+        @@guesses_left -= 1
+        @@color = "red"
         "Way too high!"
-    elsif SECRET_NUM - guess > 5
-        $color = "red"
+    elsif @@secret_num - guess > 5
+        @@guesses_left -= 1
+        @@color = "red"
         "Way too low!"
-    elsif guess > SECRET_NUM
-        $color = "#fcf235"
+    elsif guess > @@secret_num
+        @@guesses_left -= 1
+        @@color = "yellow"
         "Too high!"
-    elsif guess < SECRET_NUM
-        $color = "#fcf235"
+    elsif guess < @@secret_num
+        @@guesses_left -= 1
+        @@color = "yellow"
         "Too low!"
-    elsif guess == SECRET_NUM
-        $color = "green"
-        "You got it right! <br> <br>The secret number is #{SECRET_NUM}"
+    elsif guess == @@secret_num
+        @@correct = true
+        @@guesses_left -= 1
+        @@color = "green"
+        "You got it right! <br> <br>The secret number is #{@@secret_num}"
     end
+end
+
+def new_game
+    @@secret_num = rand(100) + 1
+    @@color = "white"
+    @@guesses_left = 5
+end
+
+def out_of_guesses?
+    @@guesses_left == 0
 end
 
 get "/" do
     guess = params['guess'].to_i
     message = check_guess(guess)
-    erb :index, :locals => {:message => message, :num => SECRET_NUM, :color => $color}
+
+    if out_of_guesses?
+        pick_num
+        new_game
+        message = "You've ran out of guesses! A new number has been generated!"
+    end
+
+    if @@correct
+        pick_num
+        new_game
+        message = "Good Job! You guessed the correct number! A new number has been generated!"
+    end
+
+    erb :index, :locals => {:message => message, :num => @@secret_num, @@color => @@color}
 
 end

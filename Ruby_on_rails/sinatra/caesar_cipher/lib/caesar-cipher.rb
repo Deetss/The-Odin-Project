4ca007081e1d9827487d237fc422688d29dd6bae
Deetss <dylan817@live.com>
2017-09-17 12:33:40 -0500

    Fixes nil case, and special character issues. Also tweaks index.erb

--- Ruby_on_rails/sinatra/caesar_cipher/lib/caesar-cipher.rb
+++ Ruby_on_rails/sinatra/caesar_cipher/lib/caesar-cipher.rb
@@ -2,32 +2,26 @@
 def caesar_cipher text, count
     new_text = ""
     count = count.to_i
-    text.each_char do |letter|
-        if letter.match(/[a-zA-Z]/)
-            count.times do |i|
-                if letter == "z"
-                    letter = "a"
-                elsif letter == "Z"
-                    letter = "A"
-                else
-                    letter = letter.next
+    if text.nil?
+        return
+    else
+        text.each_char do |letter|
+            if letter.match(/[a-zA-Z]/)
+                count.times do |i|
+                    if letter == "z"
+                        letter = "a"
+                    elsif letter == "Z"
+                        letter = "A"
+                    else
+                        letter = letter.next
+                    end
                 end
+                new_text << letter
+            else
+                new_text << letter
             end
-            new_text << letter
         end
     end
     new_text
 end
 
-# #get user input for caesar_cipher
-# puts "Welcome to the Caesar Cipher!"
-# puts "Please enter the text you wish to encrypt:"
-# user_input = gets.strip
-
-# puts "Please enter a number to encrypt by:"
-# count = gets.strip.to_i
-# #checks to make sure the user input a number
-# while count == /\D/
-#     puts "Please enter a valid number"
-#     count = gets.strip.to_i
-# end
